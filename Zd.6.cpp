725504cd96a10cd23761c7432abc5a4cb9dd341f
kirill2509-cpu <kuznetsov250900@gmail.com>
2025-09-10 00:16:14 +0700

    Add files via upload

---
+++ Zd.6.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int m;
+    int n;
+    cout << "vvedite chislo: ";
+    cin >> m;
+    cout << "vvedite chislo: ";
+    cin >> n;
+    if (m > n) {
+        cout << "number: " << m << " > " << n;
+    }
+    else if (m < n) {
+        cout << "number: " << m << " < " << n;
+    }
+    else {
+        cout << "The numbers are equal";
+    }
+}
