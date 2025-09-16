a9908e2d04479c527f0c01a161255d3e2107510a
kirill2509-cpu <kuznetsov250900@gmail.com>
2025-09-17 01:49:39 +0700

    Add files via upload

---
+++ tasks g195/tortoise_racing.c.cpp
@@ -0,0 +1,23 @@
+﻿
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> race(int v1, int v2, int g) {
+    if (v1 >= v2) return { -1, -1, -1 };
+
+    int total_seconds = g * 3600 / (v2 - v1);
+
+    int hours = total_seconds / 3600;
+    int minutes = (total_seconds % 3600) / 60;
+    int seconds = total_seconds % 60;
+
+    return { hours, minutes, seconds };
+}
+
+int main() {
+    vector<int> result = race(720, 850, 70);
+    cout << "[" << result[0] << ", " << result[1] << ", " << result[2] << "]" << endl;
+    return 0;
+}
